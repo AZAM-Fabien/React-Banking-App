7247a950aafa33d455c6358f32adc78c17bb10ad
AZAM-Fabien <azam.fabien@gmail.com>
2024-06-06 18:23:07 +0300

    try

--- ArgentBank-react/src/App.jsx
+++ ArgentBank-react/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./main.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { Provider } from "react-redux";
+// import { store } from "./store/redux.js";
+import Header from "./components/header/header.jsx";
+import Footer from "./components/footer/footer.jsx";
+import Acceuil from "./pages/acceuil.jsx";
+import Sign_in from "./pages/sign_in.jsx";
+import User from "./pages/user.jsx";
+// import Error from "./pages/Error/Error.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // <Provider store={store}>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Acceuil />} />
+          <Route path="/sign-in" element={<Sign_in />} />
+          <Route path="profile" element={<User />} />
+        </Routes>
+        <Footer />
+      </Router>
+    // </Provider>
+  );
 }
 
-export default App
+export default App;
